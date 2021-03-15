575c4d8998cfb2d6dfae5d4b33616f8c2a745b54
marinakolova <marina.kolova.359@gmail.com>
2021-03-15 21:58:09 +0200

    added solved tasks

--- JS-Advanced-May-2020/Jan2021-08-DomIntroductionExercise/08-GenerateReport/report.js
+++ JS-Advanced-May-2020/Jan2021-08-DomIntroductionExercise/08-GenerateReport/report.js
@@ -1,3 +1,24 @@
 function generateReport() {
-    //TODO
+    let checkboxes = document.querySelectorAll("input[type='checkbox']");
+
+    let selected = Array.from(checkboxes)
+        .map((x, i) => [x, i])
+        .filter(x => x[0].checked)
+        .map(x => [x[0].name, x[1]])
+
+    let rows = document.getElementsByTagName("tr");
+
+    let rowData = Array.from(rows)
+        .slice(1)
+        .map(x => Array.from(x.children).map(y => y.innerHTML))
+
+    document.getElementById("output").value = JSON.stringify(
+        rowData.map(x =>
+            selected.reduce((a, v) => {
+                const [sColName, sColIndex] = v
+                a[sColName] = x[sColIndex]
+                return a
+            }, {})
+        )
+    )
 }
