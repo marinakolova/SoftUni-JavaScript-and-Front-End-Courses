4219326dd08102d40db83c3e59a0223a65d2ef03
marinakolova <marina.kolova.359@gmail.com>
2021-04-07 09:21:54 +0300

    edit solved tasks

--- JS-Advanced-May-2020/Jan2021-20-ExamPrep2-15.02.2021/01-ChristmasGiftsDelivery/app.js
+++ JS-Advanced-May-2020/Jan2021-20-ExamPrep2-15.02.2021/01-ChristmasGiftsDelivery/app.js
@@ -1,44 +1,46 @@
 function solution() {
-    const cardLists = document.querySelectorAll(`.container section ul`)
-    const inputField = document.querySelector('input')
+    const cardLists = document.querySelectorAll(`.container section ul`);
+    const inputField = document.querySelector('input');
 
     function giftTemplFactory(moved, name) {
-        const li = document.createElement('li')
+        const li = document.createElement('li');
 
-        li.className = 'gift'
+        li.className = 'gift';
         li.innerHTML =
             `${name}${moved
                 ? `<button id="sendButton">Send</button><button id="discardButton">Discard</button>`
-                : ''}`
+                : ''}`;
 
-        return li
-    }
+        return li;
+    };
 
     const appendToSection = (s, e) => {
-        s.appendChild(giftTemplFactory(false, e.parentNode.childNodes[0].textContent))
-        e.parentNode.outerHTML = ''
-    }
+        s.appendChild(
+            giftTemplFactory(false, e.parentNode.childNodes[0].textContent));
+
+        e.parentNode.outerHTML = '';
+    };
 
     document.addEventListener('click', (e) => {
         if (e.target.tagName === 'BUTTON') {
-            const action = e.target.id
+            const action = e.target.id;
 
             const actions = {
                 '': () => {
-                    const listOfGifts = cardLists[0]
-                    const gifts = Array.from(listOfGifts.children)
+                    const listOfGifts = cardLists[0];
+                    const gifts = Array.from(listOfGifts.children);
 
-                    gifts.push(giftTemplFactory(true, inputField.value))
-                    gifts.sort((a, b) => a.innerHTML.localeCompare(b.innerHTML))
-                    gifts.forEach(gift => listOfGifts.appendChild(gift))
-                    inputField.value = ''
+                    gifts.push(giftTemplFactory(true, inputField.value));
+                    gifts.sort((a, b) => a.innerHTML.localeCompare(b.innerHTML));
+                    gifts.forEach(gift => listOfGifts.appendChild(gift));
+                    inputField.value = '';
 
                 },
                 'sendButton': () => appendToSection(cardLists[1], e.target),
                 'discardButton': () => appendToSection(cardLists[2], e.target),
             }
 
-            actions[action]()
+            actions[action]();
         }
-    })
+    });
 }
